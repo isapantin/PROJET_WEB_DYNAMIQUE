2aa5b5d2d25187ad0ca61c3ff45d2343323fe911
loisz <lois.zorgniotti.sebbagh@gmail.com>
2026-05-20 22:16:31 +0200

    maj menu

--- includes/menu.php
+++ includes/menu.php
@@ -3,11 +3,44 @@
     <h1>OmnesEvent</h1>
 
     <nav>
+
         <a href="index.php">Accueil</a>
-        <a href="profil.php">Profil</a>
-        <a href="creer_evenement.php">Créer</a>
-        <a href="connexion.php">Connexion</a>
-        <a href="inscription.php">Inscription</a>
+
+        <?php if(isset($_SESSION['id'])) { ?>
+
+            <a href="profil.php">Profil</a>
+
+        <?php } ?>
+
+        <?php
+        if(isset($_SESSION['role'])
+        && $_SESSION['role'] == 'organisateur') {
+        ?>
+
+            <a href="creer_evenement.php">
+                Créer
+            </a>
+
+        <?php } ?>
+
+        <?php if(!isset($_SESSION['id'])) { ?>
+
+            <a href="connexion.php">
+                Connexion
+            </a>
+
+            <a href="inscription.php">
+                Inscription
+            </a>
+
+        <?php } else { ?>
+
+            <a href="deconnexion.php">
+                Déconnexion
+            </a>
+
+        <?php } ?>
+
     </nav>
 
 </header>
