8df6f3e93942a032c1ad21747c6a59998d57df0b
isapa <isa.pantin@edu.ece.fr>
2026-05-20 22:34:42 +0200

    evenement detail

---
+++ evenement.php
@@ -0,0 +1,78 @@
+<?php
+
+session_start();
+
+require("includes/connexion_bdd.php");
+
+/* RECUPERATION ID */
+
+if(!isset($_GET['id'])) {
+
+    die("Événement introuvable");
+}
+
+$id = $_GET['id'];
+
+/* REQUETE SQL */
+
+$sql = "SELECT * FROM evenements
+WHERE id = $id";
+
+$resultat = mysqli_query($bdd, $sql);
+
+$event = mysqli_fetch_assoc($resultat);
+
+?>
+
+<?php include("includes/header.php"); ?>
+<?php include("includes/menu.php"); ?>
+
+<main>
+
+<section class="detail-evenement">
+
+<img src="uploads/<?php echo $event['image']; ?>">
+
+<div class="contenu-evenement">
+
+<h2>
+<?php echo $event['titre']; ?>
+</h2>
+
+<p class="categorie-detail">
+<?php echo $event['categorie']; ?>
+</p>
+
+<p>
+<?php echo $event['description']; ?>
+</p>
+
+<p>
+📍 Lieu :
+<?php echo $event['lieu']; ?>
+</p>
+
+<p>
+📅 Date :
+<?php echo $event['date_evenement']; ?>
+</p>
+
+<p>
+👥 Capacité :
+<?php echo $event['capacite']; ?> places
+</p>
+
+<a class="btn-reserver"
+href="reservation.php?id=<?php echo $event['id']; ?>">
+
+Réserver une place
+
+</a>
+
+</div>
+
+</section>
+
+</main>
+
+<?php include("includes/footer.php"); ?>
