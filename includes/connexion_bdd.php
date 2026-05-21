6e12974e7ff3b31cec1a0fcf768325508a5ab2e1
isapa <isa.pantin@edu.ece.fr>
2026-05-21 10:42:20 +0200

    responsive corr bug

--- includes/connexion_bdd.php
+++ includes/connexion_bdd.php
@@ -1,16 +1,16 @@
 <?php
 
 $bdd = mysqli_connect(
-    "fdb1031.your-hosting.net",
-    "4760659_omneseventasso",
-    "Isa123465.",
-    "4760659_omneseventasso",
-    3306
+    "localhost",
+    "root",
+    "",
+    "omnesevent"
 );
 
 if(!$bdd) {
 
-    die(mysqli_connect_error());
+    die("Erreur connexion : "
+    . mysqli_connect_error());
 }
 
 ?>
